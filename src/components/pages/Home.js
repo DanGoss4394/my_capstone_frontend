a7924e10574c4e70d5b24acf188cdbb7364950fc
Daniel Gosselin <dangoscode@gmail.com>
2021-03-26 15:40:42 -0600

    Add Home page Styles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home_container">
+      <div className="heading">
+        <h1>Home</h1>
+      </div>
+      <div className="home_wrapper"></div>
     </div>
   );
 };
 export default Home;
-
