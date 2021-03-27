05206cc5f32c57db00ba3974b82536b6e1f40c08
Daniel Gosselin <dangoscode@gmail.com>
2021-03-27 01:35:11 -0600

    Remove warnings

--- src/components/pages/Auth.js
+++ src/components/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import AuthForm from "../Auth/AuthForm";
 import LogoutBtn from "../Auth/LogoutBtn";
 
