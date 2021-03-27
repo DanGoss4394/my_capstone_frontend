2339c160824611a647363faa2d2155b200145747
Daniel Gosselin <dangoscode@gmail.com>
2021-03-26 18:20:57 -0600

    Add history to push user to home page after blog is posted

--- src/components/Blog/BlogForm.js
+++ src/components/Blog/BlogForm.js
@@ -1,12 +1,18 @@
 import React, { useState, useContext } from "react";
 import axois from "axios";
+import { useHistory } from "react-router-dom";
 
 import { API_URL } from "../../api/api";
+import AuthContext from "../contexts/AuthContext";
 
 const BlogForm = () => {
   const [title, setTitle] = useState("");
+  const [videoTitle, setVideoTitle] = useState("");
   const [content, setContent] = useState("");
   const [description, setDescription] = useState("");
+
+  const { userId } = useContext(AuthContext);
+  let history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,17 +22,19 @@ const BlogForm = () => {
       data: {
         title,
         content,
+        user_id: userId,
       },
       withCredentials: true,
     })
       .then((res) => {
+        history.push("/");
         console.log(res);
       })
       .catch((err) => console.log(err));
   };
   return (
     <div>
-      <form onClick={handleSubmit} className="blog-content-wrapper">
+      <form onSubmit={handleSubmit} className="blog-content-wrapper">
         <div className="blog_adder">
           <div className="heading">
             <h1>Blog Entery</h1>
@@ -67,8 +75,8 @@ const BlogForm = () => {
                 type="text"
                 name="title"
                 placeholder="Title.."
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                value={videoTitle}
+                onChange={(e) => setVideoTitle(e.target.value)}
               />
             </div>
             <div className="description">
